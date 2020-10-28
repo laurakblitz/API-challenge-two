d952282a89eda78cd3385da93f85859b9d9de10c
Laura Blitz <laura.k.blitz@gmail.com>
2020-10-28 16:13:31 -0400

    Added search bar

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // ********** VARIABLES ********** //
 const baseURL = "https://calendarific.com/api/v2/holidays?api_key=16497272d452815e82fb98ddf1016060f8aa4324&country=US&year=2021";
+//const section = document.querySelector('div');
 const section = document.querySelector('ul');
 
 // ********** SEARCH BAR ********** //
 
-//const searchBar = document.querySelector('form');
-//searchBar.addEventListener('search', displayResults);
+const searchForm = document.querySelector('form');
+const searchTerm = document.querySelector('input');
+const submitBtn = document.querySelector('button');
+
+searchForm.addEventListener('submit', fetchResults);
 
 // ********** FETCH ********** //
 fetch(baseURL)
@@ -17,6 +21,21 @@ fetch(baseURL)
   displayResults(json)
 });
 
+function fetchResults(e) {
+  e.preventDefault();
+  let url = `${baseURL}&month=${searchTerm.value}`;
+  console.log('URL:', url);
+  console.warn("hello")
+
+fetch(url)
+.then(function (result) {   
+  return result.json(); 
+})
+.then(function (json) {
+  console.log(json)
+  displayResults(json)
+});
+}
 
 // ********** DISPLAY RESULTS ********** //
 function displayResults(json) {
@@ -32,14 +51,17 @@ function displayResults(json) {
     //let pitcher = document.createElement('div');
     let pitcher = document.createElement('li');
     let info = document.createElement('div');
-    let name = document.createElement('h5');
+    //let name = document.createElement('h5');
+    let name = document.createElement('button');
+    //let name = document.createElement('div');
     let date = document.createElement('h6');
     let desc = document.createElement('p');
     let type = document.createElement('p');
     let hr = document.createElement('hr');
     let br = document.createElement('br');
+    let dateDescType = document.createElement('div');
+    let accordion = document.createElement('div');
     
-
     let current = summary.holidays[i];
     console.log('Current:', current);
 
@@ -49,14 +71,20 @@ function displayResults(json) {
     desc.textContent = `${current.description}`;
     type.textContent = `${current.type}`;
     section.setAttribute('class', 'container');
+    //section.setAttribute('class', 'accordion');
     //pitcher.setAttribute('class', 'row mx-lg-n5');
     info.setAttribute('class', 'col py-3 px-lg-5 border bg-light');
-    //name.setAttribute('class', 'btn btn-secondary btn-lg btn-block');
+    name.setAttribute('class', 'btn btn-outline-secondary btn-lg active');
     pitcher.setAttribute('class', 'card');
-    name.setAttribute('class', 'card-title');
+    //name.setAttribute('class', 'card-title');
     date.setAttribute('class', 'card-subtitle mb-2 text-muted');
     desc.setAttribute('class', 'card-text');
-    type.setAttribute('class', 'card-text')
+    type.setAttribute('class', 'card-text');
+    //dateDescType.setAttribute('id', 'collapseOne');
+    dateDescType.setAttribute('class', 'dateDescType');
+    dateDescType.setAttribute('class', 'card-body');
+    dateDescType.setAttribute('class', 'collapse.show');
+    dateDescType.setAttribute('data-toggle', 'collapse');
     
    
     //* ADD TO SECTION TAG *//
@@ -73,6 +101,21 @@ function displayResults(json) {
     info.appendChild(desc);
     info.appendChild(type);
     section.appendChild(br);
-    date.appendChild(hr);
+    //date.appendChild(hr);
+    section.appendChild(dateDescType);
+    pitcher.appendChild(dateDescType);
+    info.appendChild(dateDescType);
+    //dateDescType.appendChild(name);
+    //dateDescType.appendChild(br);
+    dateDescType.appendChild(hr);
+    dateDescType.appendChild(date);
+    dateDescType.appendChild(desc);
+    dateDescType.appendChild(type);
+    section.appendChild(accordion);
+    pitcher.appendChild(accordion);
+    info.appendChild(accordion);
+    //accordion.appendChild(info);
+    //accordion.appendChild(name);
+    //accordion.appendChild(dateDescType);
     } 
 }
